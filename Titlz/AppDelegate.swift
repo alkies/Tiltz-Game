1f8049242bc3c02c15cc96d89b81a9ae2c897ad5
Martin <alkies@webmail.co.za>
2018-06-14 15:20:03 +0200

    Final code

--- Titlz/AppDelegate.swift
+++ Titlz/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Fabric
+import Crashlytics
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +18,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         Thread.sleep(forTimeInterval: 2.0)
+        Fabric.with([Crashlytics.self])
+
         // Override point for customization after application launch.
         return true
     }
